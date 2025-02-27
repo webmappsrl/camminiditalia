74400c49933baaaf17f038cc1eb17ff2e7d13539
Gennaro Manzo <gennaromanzo1993@gmail.com>
2025-02-27 14:40:15 +0100

    feat: Added morphMap for UGC models from wm-package

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Eloquent\Relations\Relation;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Relation::morphMap([
+            'App\\Models\\UgcPoi' => \Wm\WmPackage\Models\UgcPoi::class,
+            'App\\Models\\UgcTrack' => \Wm\WmPackage\Models\UgcTrack::class,
+        ]);
     }
 }
