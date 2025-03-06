7e494773cab3bb731d60e1ccdbcf316f4ee898e8
Alessandro <alessandropeci@webmapp.it>
2025-03-06 16:09:30 +0100

    chore(layer): add EcTracks to LayerFeatures field
    This commit adds the ability to load and display EcTracks associated with a specific app_id in the LayerFeatures field. The EcTracks are retrieved from the database and passed as metadata to the field component.

--- nova-components/LayerFeatures/src/LayerFeatures.php
+++ nova-components/LayerFeatures/src/LayerFeatures.php
@@ -3,6 +3,7 @@
 namespace Wm\LayerFeatures;
 
 use Laravel\Nova\Fields\Field;
+use Wm\WmPackage\Models\EcTrack;
 
 class LayerFeatures extends Field
 {
@@ -12,4 +13,26 @@ class LayerFeatures extends Field
      * @var string
      */
     public $component = 'layer-features';
+
+    public function __construct($name, $layer, $attribute = null, $resolveCallback = null)
+    {
+        parent::__construct($name, $attribute, $resolveCallback);
+
+        // Carica automaticamente le EcTracks associate all'app_id del Layer
+        $this->loadEcTracks($layer->app_id);
+    }
+
+    /**
+     * Retrieve and set the EcTracks associated with the given app_id.
+     *
+     * @param int $appId
+     * @return $this
+     */
+    public function loadEcTracks($appId)
+    {
+        // Ottieni le tracce associate all'app_id specificato
+        $ecTracks = EcTrack::where('app_id', $appId)->get()->toArray();
+
+        $this->withMeta(['tracks' => $ecTracks]);
+    }
 }
