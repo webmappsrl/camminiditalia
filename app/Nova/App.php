e6016dc5640ab5651c087fedb1f40164bf3156f5
bongiu <peppedeka@gmail.com>
2025-03-28 11:40:46 +0100

    feat: add UpdateAppTracksOnAws action to App resource
    This commit adds a new action called "UpdateAppTracksOnAws" to the App resource in Nova. The action allows users to update all tracks of an app on AWS. It dispatches a job for each track and returns a message with the total number of tracks updated.

--- app/Nova/App.php
+++ app/Nova/App.php
@@ -2,6 +2,25 @@
 
 namespace App\Nova;
 
+use App\Nova\Actions\UpdateAppTracksOnAws;
 use Wm\WmPackage\Nova\App as NovaApp;
 
-class App extends NovaApp {}
+class App extends NovaApp
+{
+    /**
+     * Get the actions available for the resource.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return array
+     */
+    public function actions(\Laravel\Nova\Http\Requests\NovaRequest $request)
+    {
+        return array_merge(parent::actions($request), [
+            (new UpdateAppTracksOnAws)
+                ->onlyOnDetail()
+                ->confirmText('Sei sicuro di voler aggiornare tutte le tracks di questa app su AWS?')
+                ->confirmButtonText('Sì, aggiorna')
+                ->cancelButtonText('No, annulla'),
+        ]);
+    }
+}
