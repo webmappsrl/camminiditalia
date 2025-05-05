070e38047cbdcab53f169d4abe6051809086b8c1
Gennaro Manzo <gennaromanzo1993@gmail.com>
2025-05-05 14:24:19 +0200

    feat: added layer policy and registered custom policy for layer model in appServiceProvider

--- app/Nova/Layer.php
+++ app/Nova/Layer.php
@@ -2,14 +2,27 @@
 
 namespace App\Nova;
 
-use Laravel\Nova\Http\Requests\NovaRequest;
 use Laravel\Nova\Panel;
 use Wm\LayerFeatures\LayerFeatures;
-use Wm\WmPackage\Models\EcTrack as WmEcTrack;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Nova\Http\Requests\NovaRequest;
 use Wm\WmPackage\Nova\Layer as WmNovaLayer;
+use Wm\WmPackage\Models\EcTrack as WmEcTrack;
 
 class Layer extends WmNovaLayer
 {
+    public static function indexQuery(NovaRequest $request, $query)
+    {
+        $user = Auth::user();
+
+        if ($user && !$user->hasRole('Administrator')) {
+            return $query->where('user_id', $user->id);
+        }
+
+        return $query;
+    }
+
+
     public function fields(NovaRequest $request): array
     {
         return [
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
+use Wm\WmPackage\Models\Layer;
+use App\Policies\LayerPolicy;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +22,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::policy(Layer::class, LayerPolicy::class);
     }
 }
