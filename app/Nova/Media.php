3728a218578d23eedf8a1e68bfd0d2a446559aae
Carla Cupani <cpnclp00@gmail.com>
2025-08-18 12:11:06 +0200

    feat(nova): ✨ add custom labels for Media resource
    - Implement `label` and `singularLabel` methods in the `Media` class to provide custom labels
    - Utilize localization with `__()` function to ensure labels are translatable

--- app/Nova/Media.php
+++ app/Nova/Media.php
@@ -4,4 +4,15 @@ namespace App\Nova;
 
 use Wm\WmPackage\Nova\Media as WmNovaMedia;
 
-class Media extends WmNovaMedia {}
+class Media extends WmNovaMedia
+{
+    public static function label(): string
+    {
+        return __('Media');
+    }
+
+    public static function singularLabel(): string
+    {
+        return __('Media');
+    }
+}
