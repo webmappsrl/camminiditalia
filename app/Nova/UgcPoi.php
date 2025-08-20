67d0c51b65b2d0e5a2998e8e7baf9707ab48e824
Carla Cupani <cpnclp00@gmail.com>
2025-08-20 15:44:40 +0200

    feat(nova): ✨ add trait to hide 'app' field from index OC:6092
    - Introduced `HidesAppFromIndexTrait` to streamline the process of hiding the 'app' attribute from index views in Nova resources.
    - Updated `UgcPoi` and `UgcTrack` classes to implement the new trait, ensuring consistent application of this behavior across these resources.

--- app/Nova/UgcPoi.php
+++ app/Nova/UgcPoi.php
@@ -2,6 +2,10 @@
 
 namespace App\Nova;
 
+use App\Nova\Traits\HidesAppFromIndexTrait;
 use Wm\WmPackage\Nova\UgcPoi as WmNovaUgcPoi;
 
-class UgcPoi extends WmNovaUgcPoi {}
+class UgcPoi extends WmNovaUgcPoi
+{
+    use HidesAppFromIndexTrait;
+}
