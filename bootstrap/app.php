cb8df64e4632a209e3de27c800119bda6a751a48
Giacomo Terreni <giacomo.terreni@gmail.com>
2026-02-09 13:27:00 +0100

    tolto extend dell'applicaizone e riportato il puntamento di wm-package all'originale del branch

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Application;
-//use Wm\WmPackage\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
