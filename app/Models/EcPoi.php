cf649a245c3058c08eb3eeb0aa4c180aa19d34af
peppedeka <peppedeka@gmail.com>
2026-02-10 12:32:21 +0100

    feat(models): ✨ add EcPoi model and integrate with Nova resource
    - Introduced a new `EcPoi` model extending from `WmEcPoi` to encapsulate custom logic or properties specific to the application.
    - Updated the `EcPoi` Nova resource to use the newly created `EcPoi` model.
    - This change ensures that any future enhancements or overrides to the EcPoi model are centralized and easily maintainable.

---
+++ app/Models/EcPoi.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Wm\WmPackage\Models\Ecpoi as WmEcPoi;
+
+class EcPoi extends WmEcPoi
+{
+
+}
--- app/Nova/EcPoi.php
+++ app/Nova/EcPoi.php
@@ -3,8 +3,11 @@
 namespace App\Nova;
 
 use Wm\WmPackage\Nova\EcPoi as WmNovaEcPoi;
+use App\Models\EcPoi as EcPoiModel;
 
-class EcPoi extends WmNovaEcPoi {
+class EcPoi extends WmNovaEcPoi 
+{
+    public static $model = EcPoiModel::class;
 
     public static function label(): string
     {
