cf5d83ffbc031b3c4ec8c9587aa2da4a5bf70d6d
Giacomo Terreni <giacomo.terreni@gmail.com>
2026-02-11 17:14:33 +0100

    feat: add TaxonomyPoiTypePolicy and update service providers
    - Introduced a new `TaxonomyPoiTypePolicy` class to manage authorization for `TaxonomyPoiType` models.
    - Updated `AppServiceProvider` to register the `TaxonomyPoiTypePolicy` with the `TaxonomyPoiType` model.
    - Added `.DS_Store` to `.gitignore` to prevent macOS system files from being tracked.
    - Enhanced `NovaServiceProvider` with a new collapsible menu section for admin files.

---
+++ app/Policies/TaxonomyPoiTypePolicy.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+use App\Models\TaxonomyPoiType;
+
+class TaxonomyPoiTypePolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Perform pre-authorization checks.
+     *
+     * @param  string  $ability
+     * @return void|bool
+     */
+    public function before(User $user, $ability)
+    {
+        if ($user->email === 'team@webmapp.it') {
+            return true;
+        }
+    }
+
+    /**
+     * Determine whether the user can view any models.
+     *
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function viewAny(User $user)
+    {
+        return true;
+    
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function view(User $user, TaxonomyPoiType|\Laravel\Nova\Resource $taxonomyPoiType): bool
+    {
+        return true;
+    }
+    /**
+     * Determine whether the user can create models.
+     *
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function create(User $user)
+    {
+        return false;
+        //
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function update(User $user, TaxonomyPoiType|\Laravel\Nova\Resource $taxonomyPoiType): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function delete(User $user, TaxonomyPoiType|\Laravel\Nova\Resource $taxonomyPoiType)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     *
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function restore(User $user, TaxonomyPoiType|\Laravel\Nova\Resource $taxonomyPoiType)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     *
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function forceDelete(User $user, TaxonomyPoiType|\Laravel\Nova\Resource $taxonomyPoiType)
+    {
+        //
+    }
+}
